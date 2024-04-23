59ef845ef7ca846803fe8d7c08da60ea036be9a0
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2024-04-23 15:05:16 +0200

    uploading own xls files working

--- autoOdbiory/service_layer.py
+++ autoOdbiory/service_layer.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from werkzeug.utils import secure_filename
+
 from db_connector import DatabaseConnector
 from logic import EmailHandler
 
@@ -7,9 +9,6 @@ app.secret_key = 'sekret'
 
 
 config_file_path = '/Users/kamilgolawski/Nauka/Programowanie/pliki init/config.ini'
-email_data_file_path = '/Users/kamilgolawski/Nauka/Programowanie/Python/autoOdbiory/mcd.xlsx'
-
-email_handler = EmailHandler(config_file_path, email_data_file_path)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -20,14 +19,34 @@ def login():
         #database.connect()
         if username == 'admin':
             session['logged_in_user'] = username
-            return redirect(url_for("panel"))
+            return redirect(url_for("upload"))
         else:
             return render_template("login.html", message="Nieprawidłowe dane logowania.")
     return render_template("login.html", message=None)
 
+
+from flask import request, redirect, url_for
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    if 'logged_in_user' in session:
+        username = session['logged_in_user']
+        if request.method == "POST" and 'file' in request.files:
+            file = request.files['file']
+            if file.filename != '':
+                saved_file_path = '/Users/kamilgolawski/Nauka/Programowanie/Python/autoOdbiory/autoOdbiory/server storage/' + secure_filename(file.filename)
+                file.save(saved_file_path)
+                session['email_data_file_path'] = saved_file_path
+                return redirect(url_for("panel"))
+        return render_template("upload.html", username=username)
+    else:
+        return redirect(url_for("login"))
+
 @app.route("/panel", methods=["GET", "POST"])
 def panel():
     if 'logged_in_user' in session:
+        email_data_file_path = session.get('email_data_file_path')
+        email_handler = EmailHandler(config_file_path, email_data_file_path)
         username = session['logged_in_user']
         if request.method == "POST":
             action = request.form.get("action")
